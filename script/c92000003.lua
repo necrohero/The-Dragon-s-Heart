1bdf9ff66f9d0749f7528f269c56de0295e40589
Coyot23 <69750820+Coyot23@users.noreply.github.com>
2025-08-18 20:27:22 +0100

    Add files via upload

---
+++ script/c92000003.lua
@@ -0,0 +1,83 @@
+--Tachyon Alive
+local s,id=GetID()
+function s.initial_effect(c)
+	--Eternal Galaxy Copypasta
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+--	e1:SetCondition(s.condition)
+	e1:SetCost(Cost.SelfDiscard)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--code skips e2 for plagarism intentions. This is for the banish and attach
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCondition(s.thcon)
+--	e3:SetCost(Cost.SelfBanish)
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
+	c:RegisterEffect(e3)
+end
+s.listed_series={SET_PHOTON,SET_GALAXY}
+--function s.cfilter(c) --filters for a face up LIGHT or DARK XYZ Monster
+--	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_LIGHT+ATTRIBUTE_DARK) and c:IsType(TYPE_XYZ)
+--end
+
+function s.filter(c,e,tp) --filters for a bujin on the field
+	local pg=aux.GetMustBeMaterialGroup(tp,Group.FromCards(c),tp,nil,nil,REASON_XYZ)
+	return (#pg<=0 or (#pg==1 and pg:IsContains(c))) and c:IsFaceup() and c:IsAttribute(ATTRIBUTE_LIGHT+ATTRIBUTE_DARK)
+		and Duel.IsExistingMatchingCard(s.tachyon,tp,LOCATION_EXTRA,0,1,nil,e,tp,c,c:GetCode(),pg)
+end
+function s.tachyon(c,e,tp,mc,code,pg) --filters for a either Anime or TCG Tachyon in the Extra Deck
+	return mc:IsType(TYPE_XYZ,c,SUMMON_TYPE_XYZ,tp) and c:IsType(TYPE_XYZ) and (c:IsCode(511010107) or c:IsCode(88177324)) and mc:IsCanBeXyzMaterial(c,tp)
+		and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0 and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc) --targets a a filter
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc,e,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.thconfilter(c,e,tp,mc,code,pg) --filters for a dragon xyz on field
+	return c:IsFaceup() and c:IsRace(RACE_DRAGON) and c:IsType(TYPE_XYZ)
+end
+function s.thcon(e,tp,eg,ep,ev,re,r,rp) --condition for a dragon xyz on the field
+	return Duel.IsExistingMatchingCard(s.thconfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp) --summons Tachyon
+	local tc=Duel.GetFirstTarget()
+	local pg=aux.GetMustBeMaterialGroup(tp,Group.FromCards(tc),tp,nil,nil,REASON_XYZ)
+	if not tc or tc:IsFacedown() or not tc:IsRelateToEffect(e) or tc:IsControler(1-tp) or tc:IsImmuneToEffect(e) or #pg>1 or (#pg==1 and not pg:IsContains(tc)) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.tachyon,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,tc,tc:GetCode(),pg)
+	local sc=g:GetFirst()
+	if sc then
+		sc:SetMaterial(tc)
+		Duel.Overlay(sc,tc)
+		Duel.SpecialSummon(sc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)
+		sc:CompleteProcedure()
+	end
+end
+-- beginning of the second effect
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc) --targets a dragon xyz on the  field
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and chkc:IsAbleToRemove() and chkc:IsRace(RACE_DRAGON) and chkc:IsType(TYPE_XYZ) end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToRemove,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp) --instant banish and return
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsControler(tp) and Duel.Remove(tc,0,REASON_EFFECT|REASON_TEMPORARY)~=0 then
+		Duel.BreakEffect()
+		Duel.ReturnToField(tc)
+	end
+end
